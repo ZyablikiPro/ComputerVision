fbde9755d8e7de47a8adaf09fd19ca8ce49d5e9f
Alexandr <kolesov.alex.s@gmail.com>
2014-03-16 23:10:12 +0400

    masks

--- mask-test.py
+++ mask-test.py
@@ -1,20 +1,28 @@
 import cv2
 import numpy
 
-core_sizes = [1, 3, 9, 27]
+kernel_sizes = [1, 3, 9, 27]
 img_path = 'lenna.png'
 scale = 0.5
+rows = 2
 
 img = cv2.imread(img_path)
 img = cv2.resize(img, (0,0), fx=scale, fy=scale) 
-result_size = img.shape[0], img.shape[1] * len(core_sizes), img.shape[2]
+result_size = img.shape[0] * rows, img.shape[1] * len(kernel_sizes), img.shape[2]
 result = numpy.zeros(result_size, img.dtype)
 
-for i in xrange(len(core_sizes)):
-	mask = cv2.getGaussianKernel(core_sizes[i], -1)
-	tmp = cv2.filter2D(img, -1, mask)
-	cv2.putText(tmp,"gaussian, {a}x{a}".format(a = core_sizes[i]), (10,30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
-	result[0:img.shape[0], img.shape[1] * i:img.shape[1] * (i + 1)] = tmp
+for i in xrange(len(kernel_sizes)):
+	kernel = numpy.ones((kernel_sizes[i], kernel_sizes[i]), numpy.float32)/kernel_sizes[i]**2
+	tmp = cv2.filter2D(img, -1, kernel)
+	cv2.putText(tmp,"simple, {a}x{a}".format(a = kernel_sizes[i]), (10,30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+	row = 0
+	result[img.shape[0] * row:img.shape[0] * (row + 1), img.shape[1] * i:img.shape[1] * (i + 1)] = tmp
+
+	kernel = cv2.getGaussianKernel(kernel_sizes[i], -1)
+	tmp = cv2.filter2D(img, -1, kernel)
+	cv2.putText(tmp,"gaussian, {a}x{a}".format(a = kernel_sizes[i]), (10,30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+	row = 1
+	result[img.shape[0] * row:img.shape[0] * (row + 1), img.shape[1] * i:img.shape[1] * (i + 1)] = tmp
 
 
 cv2.imshow('w1', result)
