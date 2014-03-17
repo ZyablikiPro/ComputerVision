2df0f66b2471ef505df592c61c849031ae1a97d9
Alexandr <kolesov.alex.s@gmail.com>
2014-03-17 14:50:11 +0400

    game with python

--- mask-test.py
+++ mask-test.py
@@ -1,28 +1,34 @@
 import cv2
 import numpy
 
+def drow_image(img, dst, row, col, caption):
+	cv2.putText(img, caption, (10,30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+	dst[img.shape[0] * row:img.shape[0] * (row + 1), img.shape[1] * col:img.shape[1] * (col + 1)] = img
+
+def create_image(img, sx, sy):
+	dst_size = img.shape[0] * sx, img.shape[1] * sy, img.shape[2]
+	dst = numpy.zeros(dst_size, img.dtype)
+	return dst
+
 kernel_sizes = [1, 3, 9, 27]
 img_path = 'lenna.png'
 scale = 0.5
-rows = 2
 
 img = cv2.imread(img_path)
 img = cv2.resize(img, (0,0), fx=scale, fy=scale) 
-result_size = img.shape[0] * rows, img.shape[1] * len(kernel_sizes), img.shape[2]
-result = numpy.zeros(result_size, img.dtype)
+result = create_image(img, 3, len(kernel_sizes))
 
 for i in xrange(len(kernel_sizes)):
 	kernel = numpy.ones((kernel_sizes[i], kernel_sizes[i]), numpy.float32)/kernel_sizes[i]**2
 	tmp = cv2.filter2D(img, -1, kernel)
-	cv2.putText(tmp,"simple, {a}x{a}".format(a = kernel_sizes[i]), (10,30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
-	row = 0
-	result[img.shape[0] * row:img.shape[0] * (row + 1), img.shape[1] * i:img.shape[1] * (i + 1)] = tmp
+	drow_image(tmp, result, 0, i, "averaging, {a}x{a}".format(a = kernel_sizes[i]))
 
 	kernel = cv2.getGaussianKernel(kernel_sizes[i], -1)
 	tmp = cv2.filter2D(img, -1, kernel)
-	cv2.putText(tmp,"gaussian, {a}x{a}".format(a = kernel_sizes[i]), (10,30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
-	row = 1
-	result[img.shape[0] * row:img.shape[0] * (row + 1), img.shape[1] * i:img.shape[1] * (i + 1)] = tmp
+	drow_image(tmp, result, 1, i, "gaussian, {a}x{a}".format(a = kernel_sizes[i]))
+
+	tmp = cv2.medianBlur(img, kernel_sizes[i])
+	drow_image(tmp, result, 2, i, "median, {a}x{a}".format(a = kernel_sizes[i]))
 
 
 cv2.imshow('w1', result)
@@ -32,8 +38,13 @@ cv2.destroyAllWindows()
 
 #img = cv2.Sobel(img, -1, 1, 0)
 
-#mask = [[-1,0,1],[-2,0,2],[-1,0,1]]
-#mask = numpy.asarray(mask, numpy.float32)
+#kernel = [[-1,0,-1],[0,4,0],[-1,0,-1]]
+#kernel = numpy.asarray(kernel, numpy.float32)
+#tmp = cv2.filter2D(img, -1, kernel)
+#cv2.imshow('w2', tmp)
+
+
+
 #mask = cv2.getGaussianKernel(5, -1)
 
 #img = cv2.filter2D(img, -1, mask)
